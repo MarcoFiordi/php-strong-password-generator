8c5a05951c559ba09a172dc669da30b16c240424
Marco Fiordi <marco.fiordi97@gmail.com>
2026-07-27 16:44:55 +0200

    inizializzazione repo

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password-generator</title>
+</head>
+<body>
+    <h1>Password Generator</h1>
+    <form action="" method="GET">
+        <label for="password_length">Inserisci la lunghezza della password da generare</label>
+        <input type="number"
+        id="password_length" 
+        min="4" 
+        max="30" 
+        required>
+        <button type="submit">invia</button>
+    </form>
+</body>
+</html>
